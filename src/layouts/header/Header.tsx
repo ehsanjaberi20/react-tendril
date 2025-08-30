5f631080c53c4c699dd1edc2294682b0a4da99ac
ehsanjaberi20 <109209767+ehsanjaberi20@users.noreply.github.com>
2025-08-30 18:24:50 +0330

    #

--- src/layouts/header/Header.tsx
+++ src/layouts/header/Header.tsx
@@ -1,21 +1,38 @@
 import React from "react";
+import Logo from '../../assets/images/logo1.svg';
+import {TbCodeCircle, TbInfoCircle, TbPhoneCall, TbUser} from "react-icons/tb";
+import {GoSignIn} from "react-icons/go";
 
 const Header: React.FC = () => {
     return (
-        <header className="bg-gray-100 shadow-lg ">
-            <div className="mx-auto flex items-center justify-between py-4 px-6">
-                <div className="flex items-center gap-5">
-                <div className="text-2xl font-bold text-gray-800">
-                    ReactJs Com
-                </div>
-                <nav className="space-x-6">
-                    <a href="#" className="text-gray-600 hover:text-blue-600 transition">منوی 1</a>
-
-                    <a href="#" className="text-gray-600 hover:text-blue-600 transition">منوی 2</a>
-                    <a href="#" className="text-gray-600 hover:text-blue-600 transition">منوی 3</a>
-                </nav>
-                </div>
+        <header className="bg-gray-100 shadow-lg h-[60px] flex items-center px-6 gap-2">
+            <img src={Logo} className='w-5' alt=""/>
+            <div className="text-2xl font-bold text-gray-800 flex">
+                پیچک
             </div>
+            <nav className="space-x-6 ms-10 flex">
+                <a href="#" className="text-gray-600 hover:text-blue-600 transition flex items-center">
+                    <TbCodeCircle size={20} className='mx-1'/>
+                    <span className='self-end'>کامپوننت های آماده</span>
+                </a>
+                <a href="#" className="text-gray-600 hover:text-blue-600 transition flex items-center">
+                    <TbInfoCircle size={20} className='mx-1'/>
+                    <span className='self-end'>درباره ما</span>
+                </a>
+                <a href="#" className="text-gray-600 hover:text-blue-600 transition flex items-center">
+                    <TbPhoneCall size={20} className='mx-1'/>
+                    <span className='self-end'>ارتباط با ما</span>
+                </a>
+            </nav>
+
+            <nav className="space-x-2 ms-auto flex">
+                <a href="#" className="text-gray-600 hover:text-blue-600 transition flex items-center">
+                    <TbUser size={20} className='mx-1'/>
+                </a>
+                <a href="#" className="text-gray-600 hover:text-blue-600 transition flex items-center">
+                    <GoSignIn color='red' size={20} className='mx-1'/>
+                </a>
+            </nav>
         </header>
     );
 };
